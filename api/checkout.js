15dda59044878a07b2de693f70d22fe2c049c2be
Jiri Benes <benes.jiri@shoppingarena.eu>
2024-02-16 00:40:15 +0200

    + checkout.js

---
+++ api/checkout.js
@@ -0,0 +1,31 @@
+const stripeApi = require('./stripe');
+
+async function createCheckoutSession(req, res) {
+    const domainUrl = proces.env.WEB_APP_URL;
+    const { line_items, customer_email} = req.body;
+    // check req body has line items and email
+    if ( !line_items || !customer_email) {
+        return res.status(400).json({ error: 'missing required session parameters'});
+    }
+
+    let session;
+
+    try {
+        session = await stripeApi.checkout.sessions.create({
+           payment_method_types: ['card'],
+           mode: 'payment',
+           line_items,
+           customer_email,
+           success_url: `${domainUrl}/success?session_id={CHECKOUT_SESSION_ID}`,
+           cancel_url: `${domainUrl}/canceled`,
+           shipping_address_collection: { allowed_countries: ['FI','EE','SE']}
+        });
+        res.status(200).json({ sessionID: session.id, });
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ error: 'an error occured, unable to create sesion'});
+    }
+}
+
+module.exports = createCheckoutSession;
