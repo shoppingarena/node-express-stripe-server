3ad6c63e1a054604dff93334b061f3d0860b3acf
Jiri Benes <benes.jiri@shoppingarena.eu>
2024-02-27 17:49:39 +0200

    ready

--- api/paymentintent.js
+++ api/paymentintent.js
@@ -1,5 +1,29 @@
 const stripeAPI = require('../stripe');
 
+
+
 async function paymentIntent(req, res) {
-    
+    // pull off
+    const { cartItems, description, receipt_email, shipping } = req.body;
+    let paymentIntent;
+
+    try {
+        // going to update the payment intent variebla
+        paymentIntent = await stripeAPI.paymentIntents.create({
+            // that takes an object and inside of object we going to define our parameters
+            amount: calculateOrderAmount(cartItems),
+            currency: 'eur',
+            description,
+            payment_method_types: ['card'],
+            receipt_email,
+            shipping,
+        });
+
+        res.status(200).json({clientSecret: paymentIntent.client_secret, })
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ error: 'an error occured, unable to create payment intent'})
+    }
 }
+
+module.exports = paymentIntent;
